e4f51e643a5a5b49b5213fc131df22a18ff752f8
FengLing <m13998337244@163.com>
2022-03-26 01:21:59 +0800

    Initial commit

---
+++ src/main/java/pers/jinsuichen/cpstatistic/CpstatisticApplication.java
@@ -0,0 +1,13 @@
+package pers.jinsuichen.cpstatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpstatisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpstatisticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/jinsuichen/cpstatistic/CpstatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.jinsuichen.cpstatistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpstatisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
